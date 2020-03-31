c66579d941717c190ce332052f18981638862a3a
januush <42897654+januush@users.noreply.github.com>
2020-03-31 15:59:19 +0200

    Ex. 3 Pattern Matching with case class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,20 +8,38 @@ dla pozostałych napisów „Nie ma takiego dnia”.
 własnością stanKonta - własność ma być tylko do odczytu.
 Klasa powinna udostępniać konstruktor przyjmujący początkowy stan konta
 oraz drugi, ustawiający początkowy stan konta na 0.
+
+3.	Zdefiniuj klasę Osoba z własnościami imie i nazwisko. Stwórz kilka instancji tej klasy.
+Zdefiniuj funkcję, która przyjmuje obiekt klasy osoba i przy pomocy Pattern Matching wybiera
+i zwraca napis zawierający przywitanie danej osoby.
+Zdefiniuj 2-3 różne przywitania dla konkretnych osób (z określonym imionami lub nazwiskami) oraz jedno domyślne.
  */
 object Main {
   def main(args: Array[String]): Unit = {
 
     val weekDays: List[String] = List("Poniedzialek", "Wtorek", "Sroda", "Czwartek", "Piatek", "Sobota", "Niedziela")
 
+    //1
     println("1. PatternMatching: " + weekDays(0) + " - " + matchDay(weekDays(0)))
     println("1. PatternMatching: " + weekDays(6) + " - " + matchDay(weekDays(6))+"\n")
 
+    //2
     val bankAccount = new BankAccount()
     val newBanckAccount = new BankAccount(123.45)
 
     println("2. Stan konta z poczatkowa suma: " + bankAccount.balance)
     println("2. Stan drugiego konta z domyslna suma: " + newBanckAccount.balance)
+
+    //3
+    val person1 = new Person("Kasia","Kasiowa")
+    val person2 = new Person("Basia","Basiowa")
+    val person3 = new Person("Marysia", "Marysiowa")
+    val person4 = new Person("Adam", "Adamski")
+
+    println(s"3. Say Hello to $person1: " + sayHello(person1))
+    println(s"3. Say Hello to $person2: " + sayHello(person2))
+    println(s"3. Say Hello to $person3: " + sayHello(person3))
+    println(s"3. Say Hello to stranger: " + sayHello(person4)+"\n")
 
 
   }
@@ -51,4 +69,14 @@ object Main {
     }
   }
 
+  case class Person(val name: String, val surname: String)
+
+  def sayHello(person: Main.Person): String ={
+    person match {
+      case Person(name,surname) if name == "Kasia" => s"Halo $name $surname!"
+      case Person(name,surname) if name == "Basia" => s"Salut $name $surname!"
+      case Person(name,surname) if name == "Marysia" => s"Hi $name $surname!"
+      case default => "Hello Stranger!"
+    }
+  }
 }
